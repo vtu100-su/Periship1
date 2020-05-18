d54aeb3dabf4870bf27b7180543fba1577059a46
vtu100-su <vtu100@syr.edu>
2020-05-17 20:56:01 -0400

    Add files via upload

---
+++ Periship.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+#1  
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+            string_s= []
+            string_t= []
+   
+
+    for char in S :
+    if char == "#"
+    len(string_s) : 
+        string_s.pop()
+        else: 
+            string_s.append(char)
+            
+    
+    for char in T :
+    if char == "#"
+    len(string_t) : 
+        string_t.pop
+        else: 
+            string_t.append(char)
+            
+        reutrn string_s == string_t
+
+
+# In[4]:
+
+
+#2 Still trying to figure it out.
+
+        index_list =[]
+        for i in range(len(S))
+        if [i] == C: index_list.append(i)
+            
+            
+   
+
+    res = [] 
+    
+            
+
+        
+
+
+# In[5]:
+
+
+#3 Unsure
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph = paragraph.lower()
+         for i in paragraph :
+            if i in (!?',) 
+            
+            
+
+
+# In[6]:
+
+
+#4 A confusing question.
+
+
+# In[7]:
+
+
+#5 This one not sure.
+
+        valid = {'2','5','6','9'}
+        invalid = {'3','4','7'}
+        for num in range
+         
+        if s & invalid:
+           
+        continue 
+            elif s & valid:
+        return result
+
+
+# In[8]:
+
+
+#6 still trying to figure this out for the longest time. 
+if len(word1) != len(words) return false
+    
+
+
+# In[9]:
+
+
+#7
+return word == word.upper() or word == word.lower() or (word[0] == word[0].upper() and word[1:] == word[1:].lower())
+
+
+# In[21]:
+
+
+#8
+len(s.split())
+     count = 0 
+        return len(s.split())
+
+
+# In[19]:
+
+
+#9 I tired my best to undeertsand it.
+
+
+# In[22]:
+
+
+#10  
+if load + wights < = capacity:
+    
+
+
+# In[23]:
+
+
+#11 Really difficult
+
+
+# In[ ]:
+
+
+#12 Really difficult
+
